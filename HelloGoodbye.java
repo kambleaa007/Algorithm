c915356a1dbe1f380e2cab50477628bb7cc75ce3
kambleaa007 <ashishkamble191191@gmail.com>
2023-09-20 22:03:47 +0530

    week 1 init

--- HelloGoodbye.java
+++ HelloGoodbye.java
@@ -4,8 +4,11 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class HelloGoodbye {
     public static void main(String[] args) {
-        System.out.println("Hello " + args[0] + " and " + args[1] + ".");
+        StdOut.println(String.format("Hello %s and %s.", args[0], args[1]));
+        StdOut.println(String.format("Goodbye %s and %s.", args[1], args[0]));
     }
 }
